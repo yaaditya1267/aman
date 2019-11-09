aeec394afed458203ebd046d9c0d4210bb23645c
yaaditya1267 <54935006+yaaditya1267@users.noreply.github.com>
2019-11-09 19:28:48 +0530

    Create prg4.py

---
+++ prg4.py
@@ -0,0 +1,13 @@
+students = {'1MS17IS020':'Ahana','1MS17IS021':'Zain'}
+list = ["value1","value2","value3"]
+
+j = 0
+
+for i in students:
+    print("Key is ",i," and value is ",students[i])
+    list[j] = students[i]
+    j = j+1
+
+print(list)
+print(students.keys)
+print(students.values())
