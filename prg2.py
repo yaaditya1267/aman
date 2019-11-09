157fc52747933a1ecb563b0b4c9a57ad03ed5d28
yaaditya1267 <54935006+yaaditya1267@users.noreply.github.com>
2019-11-09 19:27:48 +0530

    Create prg2.py

---
+++ prg2.py
@@ -0,0 +1,24 @@
+print("Python has fine data types: Number, String, List, Tuple, Dictionary")
+print("\n *** Demo: String Manipulations")
+mystr = "Hungry Kya!"
+
+print(mystr)
+print(mystr[0])
+print(mystr[2:5])
+print(mystr[2:])
+print(mystr*2)
+print(mystr+" Abe Nahin")
+print("Length is ",len(mystr))
+print("Replacing ",mystr.replace("Kya","?"))
+print("Splitting ",mystr.split(" "))
+
+print("\n *** Demo Number - Python can handle integer, float and complex")
+
+x = 33
+y1 = 9.87
+y2 = 87.7e100
+z = 4j
+print(type(x))
+print(type(y2))
+print(type(y1))
+print(type(z))
